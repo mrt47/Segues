69e606d251484e3b6a5da9163fcbc9f9296e449a
Murat Kuran <muratkuran85@gmail.com>
2017-11-01 01:22:18 +0300

    Initial Commit

---
+++ SwappingScreens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by Murat Kuran on 01/11/2017.
+//  Copyright © 2017 Murat Kuran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
